199db0c576a5ae18bd5b56f626472754bec71cf5
kotekpsotek <siematumichael@protonmail.com>
2023-11-06 20:15:55 +0100

    NPM Package REEXPORTS

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,3 @@
 // Reexport your entry components here
 export { handler as serviceAdminHandler } from "./admin/build/handler.js";
-export { chatPlugin } from "./plugin.js";
+export { chatPlugin as svelteChatPlugin } from "./plugin.js";
